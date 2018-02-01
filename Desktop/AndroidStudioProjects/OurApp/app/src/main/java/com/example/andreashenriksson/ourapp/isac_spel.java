856db45a5933471df4daaf442f11c40ca78c9b28
IsacArvidsson <isac.arvidsson97@gmail.com>
2018-02-01 12:55:10 +0100

    test

--- Desktop/AndroidStudioProjects/OurApp/app/src/main/java/com/example/andreashenriksson/ourapp/isac_spel.java
+++ Desktop/AndroidStudioProjects/OurApp/app/src/main/java/com/example/andreashenriksson/ourapp/isac_spel.java
@@ -2,12 +2,17 @@ package com.example.andreashenriksson.ourapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
-public class Main2Activity extends AppCompatActivity {
+public class isac_spel extends AppCompatActivity {
+
+    public Button myButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.IsacSpel);
+        setContentView(R.layout.isac_spel);
+
+        myButton = (Button) findViewById(R.id.bt0);
     }
 }
