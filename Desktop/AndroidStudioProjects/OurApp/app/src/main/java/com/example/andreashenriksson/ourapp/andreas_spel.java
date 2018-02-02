f176acf38ea401096ea7b479f298d5fa21964650
heeenkie <andreas-henriksson@live.se>
2018-02-02 14:19:50 +0100

    first commit

--- Desktop/AndroidStudioProjects/OurApp/app/src/main/java/com/example/andreashenriksson/ourapp/andreas_spel.java
+++ Desktop/AndroidStudioProjects/OurApp/app/src/main/java/com/example/andreashenriksson/ourapp/andreas_spel.java
@@ -1,6 +1,8 @@
 package com.example.andreashenriksson.ourapp;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,14 +13,14 @@ public class andreas_spel extends AppCompatActivity {
 
 
     TextView tv_time;
-    Button b_1, b_2, b_3, b_4, b_5, b_6, b_7, b_8, b_9, b_startover;
+    Button b_1, b_2, b_3, b_4, b_5, b_6, b_7, b_8, b_9, b_startover, b_start;
+    int oldValue, newValue, time= 30, num1, num2, num3, num4, num5, num6, num7, num8, num9;
+    CountDownTimer timer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_andreas_spel);
-
-
 
         b_1 = (Button) findViewById(R.id.b_1);
         b_2 = (Button) findViewById(R.id.b_2);
@@ -30,12 +32,70 @@ public class andreas_spel extends AppCompatActivity {
         b_8 = (Button) findViewById(R.id.b_8);
         b_9 = (Button) findViewById(R.id.b_9);
         b_startover = (Button) findViewById(R.id.b_startover);
+        b_start = (Button) findViewById(R.id.b_start);
         tv_time = (TextView) findViewById(R.id.tv_time);
+
+
+
+        b_1.setEnabled(false);
+        b_2.setEnabled(false);
+        b_3.setEnabled(false);
+        b_4.setEnabled(false);
+        b_5.setEnabled(false);
+        b_6.setEnabled(false);
+        b_7.setEnabled(false);
+        b_8.setEnabled(false);
+        b_9.setEnabled(false);
+
+        timer = new CountDownTimer(30000, 1000){
+            @Override
+            public void onTick(long millisUntilFinished){
+                time--;
+                tv_time.setText("Time: " + time);
+            }
+
+            @Override
+            public void onFinish() {
+                b_1.setEnabled(false);
+                b_2.setEnabled(false);
+                b_3.setEnabled(false);
+                b_4.setEnabled(false);
+                b_5.setEnabled(false);
+                b_6.setEnabled(false);
+                b_7.setEnabled(false);
+                b_8.setEnabled(false);
+                b_9.setEnabled(false);
+
+            }
+
+        };
+
+
+        b_start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                b_1.setEnabled(true);
+                b_2.setEnabled(true);
+                b_3.setEnabled(true);
+                b_4.setEnabled(true);
+                b_5.setEnabled(true);
+                b_6.setEnabled(true);
+                b_7.setEnabled(true);
+                b_8.setEnabled(true);
+                b_9.setEnabled(true);
+                timer.start();
+            }
+            });
+
 
         b_1.setOnClickListener(new View.OnClickListener() {
             @Override
-                public void onClick(View view) {
+            public void onClick(View view) {
                 b_1.setEnabled(false);
+                if (oldValue<newValue)
+                    b_1.setBackgroundColor(Color.parseColor("#00ff00"));
+                else
+                    b_1.setBackgroundColor(Color.parseColor("#ff0000"));
             }
         });
 
@@ -43,6 +103,10 @@ public class andreas_spel extends AppCompatActivity {
             @Override
             public void onClick(View view){
                 b_2.setEnabled(false);
+                if (oldValue<newValue)
+                    b_2.setBackgroundColor(Color.parseColor("#00ff00"));
+                else
+                    b_2.setBackgroundColor(Color.parseColor("#ff0000"));
             }
         });
 
@@ -50,13 +114,22 @@ public class andreas_spel extends AppCompatActivity {
             @Override
             public void onClick(View view){
                 b_3.setEnabled(false);
+                if (oldValue<newValue)
+                    b_3.setBackgroundColor(Color.parseColor("#00ff00"));
+                else
+                    b_3.setBackgroundColor(Color.parseColor("#ff0000"));
             }
+
         });
 
         b_4.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
                 b_4.setEnabled(false);
+                if (oldValue<newValue)
+                    b_4.setBackgroundColor(Color.parseColor("#00ff00"));
+                else
+                    b_4.setBackgroundColor(Color.parseColor("#ff0000"));
             }
         });
 
@@ -64,6 +137,10 @@ public class andreas_spel extends AppCompatActivity {
             @Override
             public void onClick(View view){
                 b_5.setEnabled(false);
+                if (oldValue<newValue)
+                    b_5.setBackgroundColor(Color.parseColor("#00ff00"));
+                else
+                    b_5.setBackgroundColor(Color.parseColor("#ff0000"));
             }
         });
 
@@ -71,6 +148,10 @@ public class andreas_spel extends AppCompatActivity {
             @Override
             public void onClick(View view){
                 b_6.setEnabled(false);
+                if (oldValue<newValue)
+                    b_6.setBackgroundColor(Color.parseColor("#00ff00"));
+                else
+                    b_6.setBackgroundColor(Color.parseColor("#ff0000"));
             }
         });
 
@@ -78,6 +159,10 @@ public class andreas_spel extends AppCompatActivity {
             @Override
             public void onClick(View view){
                 b_7.setEnabled(false);
+                if (oldValue<newValue)
+                    b_7.setBackgroundColor(Color.parseColor("#00ff00"));
+                else
+                    b_7.setBackgroundColor(Color.parseColor("#ff0000"));
             }
         });
 
@@ -85,6 +170,10 @@ public class andreas_spel extends AppCompatActivity {
             @Override
             public void onClick(View view){
                 b_8.setEnabled(false);
+                if (oldValue<newValue)
+                    b_8.setBackgroundColor(Color.parseColor("#00ff00"));
+                else
+                    b_8.setBackgroundColor(Color.parseColor("#ff0000"));
             }
         });
 
@@ -92,7 +181,11 @@ public class andreas_spel extends AppCompatActivity {
             @Override
             public void onClick(View view){
                 b_9.setEnabled(false);
-                //b_9.setBackgroundColor();
+                if (oldValue<newValue)
+                    b_9.setBackgroundColor(Color.parseColor("#00ff00"));
+                else
+                    b_9.setBackgroundColor(Color.parseColor("#ff0000"));
+
             }
         });
 
@@ -109,6 +202,7 @@ public class andreas_spel extends AppCompatActivity {
                 b_7.setEnabled(true);
                 b_8.setEnabled(true);
                 b_9.setEnabled(true);
+                b_9.setBackgroundResource(android.R.drawable.btn_default);
 
             }
         });
