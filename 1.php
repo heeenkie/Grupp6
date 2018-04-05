9369bfca81746b03a4163805286f55aabf840492
Andreas <andreas-henriksson@live.se>
2018-04-05 11:12:01 +0200

    1

--- 1.php
+++ 1.php
@@ -2,21 +2,49 @@
 	include_once 'header.php';
 ?>
 <link rel="stylesheet" type="text/css" href="css/1.css">
-<div class="cont">
+<div class="img_container">
     <div class="img">
         <img src="image/kebab.jpg" alt="kebab" width="100%" height="100%">
     </div>
-	<div clss="field">
-        <div id="tf">
-        </div>
-        <div id="edu">
-        </div>
-        <div>
-        </div>
-        <div>
-        </div>
+</div>
+<div class="name">
+    Adrians Kebab
+</div>
+
+<div class="field">
+    <div id="ex">
+        <h1>Ansök här</h1>
     </div>
-	
+    <div id="tf">
+        <h1>
+            Om företaget
+        </h1>
+        <p>
+
+
+        </p>
+    </div>
+    <div id="edu">
+        <h2>Intressanta utbildningar</h2>
+        <p>
+            Industriell ekonomi
+        </p>
+    </div>
+</div>
+<div class="field" style="height: 300px;">
+    <div class="job">
+        <h1> Tidigare ex-jobb</h1>
+    </div>
+
+    <div class="job">
+        <h1> Tidigare ex-jobb</h1>
+    </div>
+</div>
+
+
+<div id="extra">
+<h1>Länkar:</h1>
+Våran hemsida
 </div>
 <?php
 
